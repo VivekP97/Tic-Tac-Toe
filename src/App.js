7b6a1273c943ea6a7b141f9ccdf286ee86d71af5
Vivek Parmar <vivekparmar97@outlook.com>
2025-02-02 22:58:08 -0500

    set up the initial game board with bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 export default function GameBoard() {
   return (
     <>
-      <div>
-        <p>Hello world!</p>
+      <div className="col-6 mx-auto card p-4 mt-3">
+        <h3 className="text-center">My Tic-Tac-Toe Game</h3>
+        <div className="d-flex flex-column align-items-center my-4">
+          <div className="d-flex">
+            <button className="square">X</button>
+            <button className="square">O</button>
+            <button className="square">X</button>
+          </div>
+          <div className="d-flex">
+            <button className="square">O</button>
+            <button className="square">X</button>
+            <button className="square">O</button>
+          </div>
+          <div className="d-flex">
+            <button className="square">X</button>
+            <button className="square">O</button>
+            <button className="square">X</button>
+          </div>
+        </div>
       </div>
     </>
   );
