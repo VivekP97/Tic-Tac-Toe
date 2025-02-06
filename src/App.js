e8ee2bd75127650cdf494291731d6727365bc191
Vivek Parmar <vivekparmar97@outlook.com>
2025-02-05 23:44:07 -0500

    move game state up to parent of Board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import Board from "./components/Board";
 
 export default function App() {
+  /* Initialize state for keeping track of user turns */
+  const [xIsNext, setXIsNext] = useState(true);
+
+  /* Initialize the history state (to keep track of moves) */
+  const [history, setHistory] = useState([
+    [null, null, null, null, null, null, null, null, null]
+  ]);
+
+  // Set the current state of the game from the history
+  const currGameState = history[history.length-1];
+
+  // Function to handle a move made by a player
+  function handleMove(newGameState) {
+    // Append the move to the history
+    setHistory([...history, newGameState]);
+
+    setXIsNext(!xIsNext);
+  }
+
+
   return (
     <>
       {/* Container for all the page content */}
@@ -9,7 +30,7 @@ export default function App() {
         <h3 className="text-center">My Tic-Tac-Toe Game</h3>
 
         {/* Tic-Tac-Toe Board */}
-        <Board />
+        <Board xIsNext={xIsNext} currGameState={currGameState} handleMove={handleMove} />
       </div>
     </>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,11 @@
 import Square from "./Square";
 import { useState } from "react";
 
-export default function Board() {
-  /* Initialize the board data to keep track of the game state */
-  const [gameState, setGameState] = useState([null, null, null, null, null, null, null, null, null]);
-
-  /* Initialize state for keeping track of user turns */
-  const [xIsNext, setXIsNext] = useState(true);
+export default function Board({xIsNext, currGameState, handleMove}) {
 
   /* This function is triggered when a square is clicked */
   function handleSquareClick(i) {
-    const newGameState = gameState.slice();
+    const newGameState = currGameState.slice();
 
     // Ensure the selected square was not already set
     if (newGameState[i] !== null || calculateWinner(newGameState) !== null) {
@@ -20,19 +15,18 @@ export default function Board() {
 
     // Set the next value on the selected square
     newGameState[i] = xIsNext ? "X" : "O";
-    setGameState(newGameState);
 
-    // Toggle 'xIsNext'
-    setXIsNext(!xIsNext)
+    // Execute the callback from the parent to process this move
+    handleMove(newGameState);
   }
 
   // Check for a winner
   let currStatus = "";
-  const winner = calculateWinner(gameState);
+  const winner = calculateWinner(currGameState);
   if (!!winner) {
     // There is a winner.
     currStatus = "The winner is '" + winner + "'!";
-  } else if (!boardHasMoves(gameState)) {
+  } else if (!boardHasMoves(currGameState)) {
     // The board does not have any remaining moves.
     currStatus = "The game is a draw!";
   } else {
@@ -48,23 +42,23 @@ export default function Board() {
       <div className="d-flex flex-column align-items-center my-4">
         {/* Row 1 */}
         <div className="d-flex">
-          <Square value={gameState[0]} onSquareClick={() => handleSquareClick(0)} />
-          <Square value={gameState[1]} onSquareClick={() => handleSquareClick(1)} />
-          <Square value={gameState[2]} onSquareClick={() => handleSquareClick(2)} />
+          <Square value={currGameState[0]} onSquareClick={() => handleSquareClick(0)} />
+          <Square value={currGameState[1]} onSquareClick={() => handleSquareClick(1)} />
+          <Square value={currGameState[2]} onSquareClick={() => handleSquareClick(2)} />
         </div>
 
         {/* Row 2 */}
         <div className="d-flex">
-          <Square value={gameState[3]} onSquareClick={() => handleSquareClick(3)} />
-          <Square value={gameState[4]} onSquareClick={() => handleSquareClick(4)} />
-          <Square value={gameState[5]} onSquareClick={() => handleSquareClick(5)} />
+          <Square value={currGameState[3]} onSquareClick={() => handleSquareClick(3)} />
+          <Square value={currGameState[4]} onSquareClick={() => handleSquareClick(4)} />
+          <Square value={currGameState[5]} onSquareClick={() => handleSquareClick(5)} />
         </div>
 
         {/* Row 3 */}
         <div className="d-flex">
-          <Square value={gameState[6]} onSquareClick={() => handleSquareClick(6)} />
-          <Square value={gameState[7]} onSquareClick={() => handleSquareClick(7)} />
-          <Square value={gameState[8]} onSquareClick={() => handleSquareClick(8)} />
+          <Square value={currGameState[6]} onSquareClick={() => handleSquareClick(6)} />
+          <Square value={currGameState[7]} onSquareClick={() => handleSquareClick(7)} />
+          <Square value={currGameState[8]} onSquareClick={() => handleSquareClick(8)} />
         </div>
       </div>
     </>
